447ec87ae6da6b3b4672eae9bf6fa6503cc45438
Sean MacAvaney <sean.macavaney@gmail.com>
2021-02-16 17:22:23 +0000

    bumping version

--- ir_datasets/__init__.py
+++ ir_datasets/__init__.py
@@ -43,4 +43,4 @@ def main_cli():
     import sys
     main(sys.argv[1:])
 
-__version__ = "0.1.7" # NOTE: keep this in sync with setup.py
+__version__ = "0.2.0" # NOTE: keep this in sync with setup.py
