49a71fd75991e440f880773cc705ce36e71e729e
Sean MacAvaney <sean.macavaney@gmail.com>
2021-04-30 11:58:05 +0100

    Added TREC News background linking from 2018--2020.
    I don't have a copy of the v3 wapo dataset, used for 2020, though, so docs are missing.
    
    #43

--- ir_datasets/datasets/wapo.py
+++ ir_datasets/datasets/wapo.py
@@ -18,7 +18,21 @@ CORE_QREL_DEFS = {
     2: "highly relevant",
 }
 
-RM_TAGS = [' </num>', 'Narrative\n']
+BL_QREL_DEFS = {
+    0: 'The document provides little or no useful background information.',
+    2: 'The document provides some useful background or contextual information that would help the user understand the broader story context of the target article.',
+    4: 'The document provides significantly useful background ...',
+    8: 'The document provides essential useful background ...',
+    16: 'The document _must_ appear in the sidebar otherwise critical context is missing.',
+}
+
+RM_TAGS = [' </num>', 'Narrative\n', '</docid>', '</url>']
+
+BL_MAP = {
+    ' *<num> Number: ': 'query_id',
+    ' *<docid>': 'doc_id',
+    ' *<url>': 'url',
+}
 
 
 class WapoDocMedia(NamedTuple):
@@ -37,6 +51,12 @@ class WapoDoc(NamedTuple):
     body: str
     body_paras_html: Tuple[str, ...]
     body_media: Tuple[WapoDocMedia, ...]
+
+
+class TrecBackgroundLinkingQuery(NamedTuple):
+    query_id: str
+    doc_id: str
+    url: str
 
 
 class WapoDocs(BaseDocs):
@@ -132,19 +152,36 @@ def _init():
     dlc = DownloadConfig.context(NAME, base_path)
     documentation = YamlDocumentation(f'docs/{NAME}.yaml')
 
-    collection = WapoDocs(dlc['v2'])
+    collection_v2 = WapoDocs(dlc['v2'])
 
     base = Dataset(documentation('_'))
 
     subsets['v2'] = Dataset(
-        collection,
+        collection_v2,
         documentation('v2'))
 
     subsets['v2/trec-core-2018'] = Dataset(
-        collection,
+        collection_v2,
         TrecQueries(dlc['trec-core-2018/queries'], namespace='trec-core-2018', lang='en', remove_tags=RM_TAGS),
         TrecQrels(dlc['trec-core-2018/qrels'], CORE_QREL_DEFS),
         documentation('v2/trec-core-2018'))
+
+    subsets['v2/trec-news-2018'] = Dataset(
+        collection_v2,
+        TrecQueries(dlc['trec-news-2018/queries'], namespace='trec-news-2018', lang='en', qtype=TrecBackgroundLinkingQuery, qtype_map=BL_MAP, remove_tags=RM_TAGS),
+        TrecQrels(dlc['trec-news-2018/qrels'], BL_QREL_DEFS),
+        documentation('v2/trec-news-2018'))
+
+    subsets['v2/trec-news-2019'] = Dataset(
+        collection_v2,
+        TrecQueries(dlc['trec-news-2019/queries'], namespace='trec-news-2019', lang='en', qtype=TrecBackgroundLinkingQuery, qtype_map=BL_MAP, remove_tags=RM_TAGS),
+        TrecQrels(dlc['trec-news-2019/qrels'], BL_QREL_DEFS),
+        documentation('v2/trec-news-2019'))
+
+    subsets['v3/trec-news-2020'] = Dataset(
+        TrecQueries(dlc['trec-news-2020/queries'], namespace='trec-news-2020', lang='en', qtype=TrecBackgroundLinkingQuery, qtype_map=BL_MAP, remove_tags=RM_TAGS),
+        TrecQrels(dlc['trec-news-2020/qrels'], BL_QREL_DEFS),
+        documentation('v3/trec-news-2020'))
 
     ir_datasets.registry.register(NAME, base)
     for s in sorted(subsets):
