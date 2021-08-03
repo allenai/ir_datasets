b74894441da68fe32cff2730a6706f2c5d2900ba
Sean MacAvaney <sean.macavaney@gmail.com>
2021-08-03 18:41:34 -0400

    migration of documentation to https://github.com/seanmacavaney/ir-datasets.com/
    fixes #84

--- ir_datasets/commands/__init__.py
+++ ir_datasets/commands/__init__.py
@@ -2,7 +2,6 @@ from . import doc_fifos
 from . import export
 from . import lookup
 from . import list as list_cmd
-from . import documentation
 from . import build_clueweb_warc_indexes
 from . import build_download_cache
 from . import build_c4_checkpoints
@@ -13,7 +12,6 @@ COMMANDS = {
     'export': export.main,
     'lookup': lookup.main,
     'list': list_cmd.main,
-    'documentation': documentation.main,
     'build_clueweb_warc_indexes': build_clueweb_warc_indexes.main,
     'build_c4_checkpoints': build_c4_checkpoints.main,
     'build_download_cache': build_download_cache.main,
