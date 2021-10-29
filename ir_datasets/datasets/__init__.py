5e83e4f855bcc1d9d2bd7a78285bc440d15b1adb
Jan Heinrich Reimer <heinrich@reimer.family>
2021-10-29 15:14:57 +0200

    Add args.me datasets

--- ir_datasets/datasets/__init__.py
+++ ir_datasets/datasets/__init__.py
@@ -2,6 +2,7 @@ from . import base
 from . import antique
 from . import aol_ia
 from . import aquaint
+from . import argsme
 from . import beir
 from . import c4
 from . import car
---
+++ ir_datasets/datasets/argsme.py
@@ -0,0 +1,51 @@
+from ir_datasets import Dataset, registry
+from ir_datasets.datasets.base import YamlDocumentation
+from ir_datasets.formats.argsme import ArgsMeArguments
+from ir_datasets.util import DownloadConfig, home_path, Cache, ZipExtract
+
+NAME = "argsme"
+
+SUBSETS = {
+    'debateorg': (338620, "en"),
+    'debatepedia': (21197, "en"),
+    'debatewise': (14353, "en"),
+    'idebate': (13522, "en"),
+    'parliamentary': (48, "en"),
+}
+
+
+def _init():
+    base_path = home_path() / NAME
+
+    documentation = YamlDocumentation(f"docs/{NAME}.yaml")
+    download_config = DownloadConfig.context(NAME, base_path)
+
+    base = Dataset(documentation('_'))
+
+    subsets = {
+        name: Dataset(
+            ArgsMeArguments(
+                Cache(
+                    ZipExtract(
+                        download_config[name],
+                        f"{name}.json"
+                    ),
+                    base_path / f"{name}.json"
+                ),
+                namespace=f"{NAME}/{name}",
+                language=language,
+                count_hint=count_hint
+            ),
+            documentation(name)
+        )
+        for name, (count_hint, language) in SUBSETS.items()
+    }
+
+    registry.register(NAME, base)
+    for name, arguments in subsets.items():
+        registry.register(f'{NAME}/{name}', arguments)
+
+    return base, subsets
+
+
+dataset = _init()
