117b56b1b0a9ce8b24b7e33500866897a18b2dc9
Jan Heinrich Reimer <heinrich@reimer.family>
2021-11-05 01:11:23 +0100

    Add combined subset

--- ir_datasets/datasets/argsme.py
+++ ir_datasets/datasets/argsme.py
@@ -1,16 +1,35 @@
+from itertools import chain
+from typing import Dict
+
 from ir_datasets import Dataset, registry
 from ir_datasets.datasets.base import YamlDocumentation
-from ir_datasets.formats.argsme import ArgsMeArguments
+from ir_datasets.formats.argsme import ArgsMeArguments, ArgsMeCombinedArguments
 from ir_datasets.util import DownloadConfig, home_path, Cache, ZipExtract
 
 NAME = "argsme"
 
 SUBSETS = {
+    '1.0': (387692, "en", "args-me.json"),
+    '1.0-cleaned': (382545, "en", "args-me-1.0-cleaned.json"),
     '2020-04-01/debateorg': (338620, "en", "debateorg.json"),
     '2020-04-01/debatepedia': (21197, "en", "debatepedia.json"),
     '2020-04-01/debatewise': (14353, "en", "debatewise.json"),
     '2020-04-01/idebate': (13522, "en", "idebate.json"),
     '2020-04-01/parliamentary': (48, "en", "parliamentary.json"),
+}
+
+COMBINED_SUBSETS = {
+    '2020-04-01': (
+        [
+            '2020-04-01/debateorg',
+            '2020-04-01/debatepedia',
+            '2020-04-01/debatewise',
+            '2020-04-01/idebate',
+            '2020-04-01/parliamentary'
+        ],
+        387740,
+        "en"
+    ),
 }
 
 
@@ -22,30 +41,54 @@ def _init():
 
     base = Dataset(documentation('_'))
 
-    subsets = {
-        name: Dataset(
-            ArgsMeArguments(
-                Cache(
-                    ZipExtract(
-                        download_config[name],
-                        zip_path
-                    ),
-                    base_path / f"{name}.json"
+    # Arguments that can be loaded from Zenodo.
+    arguments: Dict[str, ArgsMeArguments] = {
+        name: ArgsMeArguments(
+            Cache(
+                ZipExtract(
+                    download_config[name],
+                    zip_path
                 ),
-                namespace=f"{NAME}/{name}",
-                language=language,
-                count_hint=count_hint
+                base_path / f"{name}.json"
             ),
-            documentation(name)
+            namespace=f"{NAME}/{name}",
+            language=language,
+            count_hint=count_hint
         )
-        for name, (count_hint, language, zip_path) in SUBSETS.items()
+        for name, (count_hint, language, zip_path)
+        in SUBSETS.items()
     }
 
+    # Arguments that are combined versions of other subsets.
+    combined_arguments: Dict[str, ArgsMeCombinedArguments] = {
+        name: ArgsMeCombinedArguments(
+            [arguments[subset_name] for subset_name in subset_names],
+            namespace=f"{NAME}/{name}",
+            language=language,
+            count_hint=count_hint
+        )
+        for name, (subset_names, count_hint, language)
+        in COMBINED_SUBSETS.items()
+    }
+
+    # Wrap in datasets with documentation.
+    datasets = {
+        name: Dataset(
+            arguments,
+            documentation(name)
+        )
+        for name, arguments in chain(
+            arguments.items(),
+            combined_arguments.items()
+        )
+    }
+
+    # Register datasets.
     registry.register(NAME, base)
-    for name, arguments in subsets.items():
+    for name, arguments in datasets.items():
         registry.register(f'{NAME}/{name}', arguments)
 
-    return base, subsets
+    return base, datasets
 
 
 dataset = _init()
