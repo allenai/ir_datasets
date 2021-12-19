a61340238b2b0c880b545f5dc16f283349265d03
Sean MacAvaney <sean.macavaney@gmail.com>
2021-12-19 15:54:35 +0000

    new data available for mmarco (scoreddocs, docpairs, and dev/small)

--- ir_datasets/datasets/mmarco.py
+++ ir_datasets/datasets/mmarco.py
@@ -2,10 +2,10 @@ import io
 import codecs
 import re
 import ir_datasets
-from ir_datasets.util import DownloadConfig
-from ir_datasets.datasets.base import Dataset, YamlDocumentation
+from ir_datasets.util import DownloadConfig, Lazy
+from ir_datasets.datasets.base import Dataset, YamlDocumentation, FilteredQueries
 from ir_datasets.datasets import msmarco_passage
-from ir_datasets.formats import TsvQueries, TsvDocs
+from ir_datasets.formats import TsvQueries, TsvDocs, TrecQrels, TsvDocPairs, TrecScoredDocs
 
 NAME = 'mmarco'
 
@@ -16,7 +16,6 @@ QRELS_DEFS = {
 }
 
 
-
 def _init():
     documentation = YamlDocumentation(f'docs/{NAME}.yaml')
     base_path = ir_datasets.util.home_path()/NAME
@@ -25,7 +24,10 @@ def _init():
     subsets = {}
 
     train_qrels = ir_datasets.registry['msmarco-passage/train'].qrels_handler()
-    dev_qrels = ir_datasets.registry['msmarco-passage/dev'].qrels_handler()
+    train_docparis = TsvDocPairs(dlc['train/triples'])
+    dev_qrels = TrecQrels(dlc['dev/qrels'], QRELS_DEFS)
+    dev_small_qrels = TrecQrels(dlc['dev/qrels-small'], QRELS_DEFS)
+    small_dev_qids = Lazy(lambda: {q.query_id for q in dev_small_qrels.qrels_iter()})
 
     for lang in ['es', 'fr', 'pt', 'it', 'id', 'de', 'ru', 'zh']:
         collection = TsvDocs(dlc[f'{lang}/docs'], namespace=f'mmarco/{lang}', lang=lang, count_hint=ir_datasets.util.count_hint(f'{NAME}/{lang}'))
@@ -34,23 +36,37 @@ def _init():
             collection,
             TsvQueries(dlc[f'{lang}/queries/train'], namespace=f'mmarco/{lang}', lang=lang),
             train_qrels,
+            train_docparis,
             documentation(f'{lang}/train'))
         subsets[f'{lang}/dev'] = Dataset(
             collection,
             TsvQueries(dlc[f'{lang}/queries/dev'], namespace=f'mmarco/{lang}', lang=lang),
             dev_qrels,
             documentation(f'{lang}/dev'))
+        subsets[f'{lang}/dev/small'] = Dataset(
+            collection,
+            FilteredQueries(subsets[f'{lang}/dev'].queries_handler(), small_dev_qids, mode='include'),
+            dev_small_qrels,
+            TrecScoredDocs(dlc[f'{lang}/scoreddocs/dev']) if lang not in ('zh', 'pt') else None,
+            documentation(f'{lang}/dev/small'))
         if lang in ('zh', 'pt'):
             subsets[f'{lang}/dev/v1.1'] = Dataset(
                 collection,
                 TsvQueries(dlc[f'{lang}/queries/dev/v1.1'], namespace=f'mmarco/{lang}', lang=lang),
                 dev_qrels,
                 documentation(f'{lang}/dev/v1.1'))
+            subsets[f'{lang}/dev/small/v1.1'] = Dataset(
+                collection,
+                FilteredQueries(subsets[f'{lang}/dev/v1.1'].queries_handler(), small_dev_qids, mode='include'),
+                dev_small_qrels,
+                TrecScoredDocs(dlc[f'{lang}/scoreddocs/dev/v1.1']),
+                documentation(f'{lang}/dev/v1.1'))
         if lang in ('pt',):
             subsets[f'{lang}/train/v1.1'] = Dataset(
                 collection,
                 TsvQueries(dlc[f'{lang}/queries/train/v1.1'], namespace=f'mmarco/{lang}', lang=lang),
                 train_qrels,
+                train_docparis,
                 documentation(f'{lang}/train/v1.1'))
 
     ir_datasets.registry.register(NAME, Dataset(documentation('_')))
--- test/integration/mmarco.py
+++ test/integration/mmarco.py
@@ -1,7 +1,7 @@
 import re
 import unittest
 import ir_datasets
-from ir_datasets.formats import GenericDoc, GenericQuery, TrecQrel
+from ir_datasets.formats import GenericDoc, GenericQuery, TrecQrel, GenericScoredDoc
 from .base import DatasetIntegrationTest
 
 
@@ -62,6 +62,11 @@ class TestMMarco(DatasetIntegrationTest):
             9: GenericQuery('1048587', '¿Qué es el patrón?'),
             101091: GenericQuery('524285', 'Cinta de correr inclinada significado'),
         })
+        self._test_queries('mmarco/es/dev/small', count=6980, items={
+            0: GenericQuery('1048585', '¿Qué es el hermano de Paula Deen?'),
+            9: GenericQuery('524699', 'Número del servicio de tricare'),
+            6979: GenericQuery('1048565', 'que juega michaelis sebastian'),
+        })
         self._test_queries('mmarco/fr/train', count=808731, items={
             0: GenericQuery('121352', 'Définition extrême'),
             9: GenericQuery('492875', 'Température du désinfectant'),
@@ -71,6 +76,11 @@ class TestMMarco(DatasetIntegrationTest):
             0: GenericQuery('1048578', 'Coût des piscines sans fin / spa de natation'),
             9: GenericQuery('1048587', "Qu'est-ce que le patron"),
             101092: GenericQuery('524285', "Sens de l'inclinaison du tapis roulant"),
+        })
+        self._test_queries('mmarco/fr/dev/small', count=6980, items={
+            0: GenericQuery('1048585', "Qu'est-ce que le frère de Paula Deen?"),
+            9: GenericQuery('524699', 'Numéro de service tricare'),
+            6979: GenericQuery('1048565', 'Qui joue des michaélis sébastes'),
         })
         self._test_queries('mmarco/pt/train', count=811690, items={
             0: GenericQuery('121352', 'Definir extremo'),
@@ -86,6 +96,11 @@ class TestMMarco(DatasetIntegrationTest):
             30: GenericQuery('52', 'Sabendo disso, explique por que essa condição é chamada pneumotórax.'),
             101618: GenericQuery('524285', 'Esteira inclinada significando'),
         })
+        self._test_queries('mmarco/pt/dev/small', count=7000, items={
+            0: GenericQuery('1048585', 'O que é o irmão de Paula Deen?'),
+            9: GenericQuery('524699', 'Número do serviço tricare.'),
+            6999: GenericQuery('1048565', 'que joga sebastiano michaelis'),
+        })
         self._test_queries('mmarco/pt/train/v1.1', count=808731, items={
             0: GenericQuery('121352', 'Definir extremo'),
             9: GenericQuery('492875', 'Temperatura do desinfectante'),
@@ -98,6 +113,11 @@ class TestMMarco(DatasetIntegrationTest):
             29: GenericQuery('52', 'Pneumo é um prefixo que significa ar. Sabendo disso, explique por que essa condição é chamada pneumotórax.'),
             101092: GenericQuery('524285', 'Esteira inclinada significando'),
         })
+        self._test_queries('mmarco/pt/dev/small/v1.1', count=6980, items={
+            0: GenericQuery('1048585', 'O que é o irmão de Paula Deen?'),
+            9: GenericQuery('524699', 'Número do serviço tricare.'),
+            6979: GenericQuery('1048565', 'que joga sebastiano michaelis'),
+        })
         self._test_queries('mmarco/it/train', count=808731, items={
             0: GenericQuery('121352', "L'ente creditizio definisce l'estremo"),
             9: GenericQuery('492875', 'Temperatura di sanificante di polistirolo'),
@@ -107,6 +127,11 @@ class TestMMarco(DatasetIntegrationTest):
             0: GenericQuery('1048578', 'Costo delle infinite piscine/piscine termali'),
             9: GenericQuery('1048587', '[47] ciò che è patrono'),
             101092: GenericQuery('524285', 'Tapis roulant inclinazione significato'),
+        })
+        self._test_queries('mmarco/it/dev/small', count=6980, items={
+            0: GenericQuery('1048585', "Cos'è il fratello di Paula deen?"),
+            9: GenericQuery('524699', 'Numero di servizio di tricare del benzile'),
+            6979: GenericQuery('1048565', 'Coccodrillo che gioca a michaelis Sebastian'),
         })
         self._test_queries('mmarco/id/train', count=808731, items={
             0: GenericQuery('121352', 'Definisi ekstrim'),
@@ -118,6 +143,11 @@ class TestMMarco(DatasetIntegrationTest):
             9: GenericQuery('1048587', 'Apa yang menjadi pelindungnya?'),
             101092: GenericQuery('524285', 'Arti dari treadmill treadmill'),
         })
+        self._test_queries('mmarco/id/dev/small', count=6980, items={
+            0: GenericQuery('1048585', 'Apa itu kakak paula deen'),
+            9: GenericQuery('524699', 'Nomor layanan tricare'),
+            6979: GenericQuery('1048565', 'Yang memainkan Sebastian Michaelis.'),
+        })
         self._test_queries('mmarco/de/train', count=808731, items={
             0: GenericQuery('121352', 'Bestimmen Sie extreme'),
             9: GenericQuery('492875', 'Temperatur des Reinigungsmittels'),
@@ -128,6 +158,11 @@ class TestMMarco(DatasetIntegrationTest):
             9: GenericQuery('1048587', 'Was ist der Schutzpatron?'),
             101092: GenericQuery('524285', 'Laufband Neigung Bedeutung'),
         })
+        self._test_queries('mmarco/de/dev/small', count=6980, items={
+            0: GenericQuery('1048585', 'Was ist Paula deens Bruder?'),
+            9: GenericQuery('524699', 'Nummer des Tricare-Services'),
+            6979: GenericQuery('1048565', 'Wer spielt sebastian michaelis'),
+        })
         self._test_queries('mmarco/ru/train', count=808731, items={
             0: GenericQuery('121352', '□ определить крайность'),
             9: GenericQuery('492875', '▸ Температура обеззараживающих веществ'),
@@ -137,6 +172,11 @@ class TestMMarco(DatasetIntegrationTest):
             0: GenericQuery('1048578', '- стоимость бесконечных пулов/плавучих спа'),
             9: GenericQuery('1048587', '□ что является покровителем'),
             101092: GenericQuery('524285', '&quot; беговая дорожка &quot; означает &quot; наклонная дорожка &quot;.'),
+        })
+        self._test_queries('mmarco/ru/dev/small', count=6980, items={
+            0: GenericQuery('1048585', 'Что такое брат Паулы Дина?'),
+            9: GenericQuery('524699', '□ номер службы трехразового ухода'),
+            6979: GenericQuery('1048565', 'который играет себастьяна Михаэлиса'),
         })
         self._test_queries('mmarco/zh/train', count=808731, items={
             0: GenericQuery('121352', '定义极端'),
@@ -149,11 +189,21 @@ class TestMMarco(DatasetIntegrationTest):
             9: GenericQuery('1048587', '什么是赞助人?'),
             101092: GenericQuery('524285', '踢踏机嵌入的含義Name'),
         })
+        self._test_queries('mmarco/zh/dev/small', count=6980, items={
+            0: GenericQuery('1048585', '{\\fn黑体\\fs22\\bord1\\shad0\\3aHBE\\4aH00\\fscx67\\fscy66\\2cHFFFFFF\\3cH808080}Paula Deen的弟弟是什么 {\\fn黑体\\fs22\\bord1\\shad0\\3aHBE\\4aH00\\fscx67\\fscy66\\2cHFFFFFF\\3cH808080}'),
+            9: GenericQuery('524699', '三角护理服务数量@ info: whatsthis'),
+            6979: GenericQuery('1048565', '扮演塞巴斯蒂安·米切利斯'),
+        })
         self._test_queries('mmarco/zh/dev/v1.1', count=101093, items={
             0: GenericQuery('1048578', '無止境的池塘/游泳垃圾邮件的成本'),
             1: GenericQuery('1048579', ' . '),
             9: GenericQuery('1048587', '什么是赞助人?'),
             101092: GenericQuery('524285', '踢踏机嵌入的含義Name'),
+        })
+        self._test_queries('mmarco/zh/dev/small/v1.1', count=6980, items={
+            0: GenericQuery('1048585', '{\\fn黑体\\fs22\\bord1\\shad0\\3aHBE\\4aH00\\fscx67\\fscy66\\2cHFFFFFF\\3cH808080}Paula Deen的弟弟是什么 {\\fn黑体\\fs22\\bord1\\shad0\\3aHBE\\4aH00\\fscx67\\fscy66\\2cHFFFFFF\\3cH808080}'),
+            9: GenericQuery('524699', '三角护理服务数量@ info: whatsthis'),
+            6979: GenericQuery('1048565', '扮演塞巴斯蒂安·米切利斯'),
         })
 
     def test_qrels(self):
@@ -167,6 +217,11 @@ class TestMMarco(DatasetIntegrationTest):
             9: TrecQrel('300674', '7067032', 1, '0'),
             59272: TrecQrel('371455', '8009476', 1, '0'),
         })
+        self._test_qrels('mmarco/es/dev/small', count=7437, items={
+            0: TrecQrel('300674', '7067032', 1, '0'),
+            9: TrecQrel('54544', '7068203', 1, '0'),
+            7436: TrecQrel('195199', '8009377', 1, '0'),
+        })
         self._test_qrels('mmarco/fr/train', count=532761, items={
             0: TrecQrel('1185869', '0', 1, '0'),
             9: TrecQrel('186154', '1160', 1, '0'),
@@ -176,6 +231,11 @@ class TestMMarco(DatasetIntegrationTest):
             0: TrecQrel('1102432', '2026790', 1, '0'),
             9: TrecQrel('300674', '7067032', 1, '0'),
             59272: TrecQrel('371455', '8009476', 1, '0'),
+        })
+        self._test_qrels('mmarco/fr/dev/small', count=7437, items={
+            0: TrecQrel('300674', '7067032', 1, '0'),
+            9: TrecQrel('54544', '7068203', 1, '0'),
+            7436: TrecQrel('195199', '8009377', 1, '0'),
         })
         self._test_qrels('mmarco/it/train', count=532761, items={
             0: TrecQrel('1185869', '0', 1, '0'),
@@ -187,6 +247,11 @@ class TestMMarco(DatasetIntegrationTest):
             9: TrecQrel('300674', '7067032', 1, '0'),
             59272: TrecQrel('371455', '8009476', 1, '0'),
         })
+        self._test_qrels('mmarco/it/dev/small', count=7437, items={
+            0: TrecQrel('300674', '7067032', 1, '0'),
+            9: TrecQrel('54544', '7068203', 1, '0'),
+            7436: TrecQrel('195199', '8009377', 1, '0'),
+        })
         self._test_qrels('mmarco/id/train', count=532761, items={
             0: TrecQrel('1185869', '0', 1, '0'),
             9: TrecQrel('186154', '1160', 1, '0'),
@@ -196,6 +261,11 @@ class TestMMarco(DatasetIntegrationTest):
             0: TrecQrel('1102432', '2026790', 1, '0'),
             9: TrecQrel('300674', '7067032', 1, '0'),
             59272: TrecQrel('371455', '8009476', 1, '0'),
+        })
+        self._test_qrels('mmarco/id/dev/small', count=7437, items={
+            0: TrecQrel('300674', '7067032', 1, '0'),
+            9: TrecQrel('54544', '7068203', 1, '0'),
+            7436: TrecQrel('195199', '8009377', 1, '0'),
         })
         self._test_qrels('mmarco/pt/train', count=532761, items={
             0: TrecQrel('1185869', '0', 1, '0'),
@@ -207,6 +277,11 @@ class TestMMarco(DatasetIntegrationTest):
             9: TrecQrel('300674', '7067032', 1, '0'),
             59272: TrecQrel('371455', '8009476', 1, '0'),
         })
+        self._test_qrels('mmarco/pt/dev/small', count=7437, items={
+            0: TrecQrel('300674', '7067032', 1, '0'),
+            9: TrecQrel('54544', '7068203', 1, '0'),
+            7436: TrecQrel('195199', '8009377', 1, '0'),
+        })
         self._test_qrels('mmarco/pt/train/v1.1', count=532761, items={
             0: TrecQrel('1185869', '0', 1, '0'),
             9: TrecQrel('186154', '1160', 1, '0'),
@@ -216,6 +291,11 @@ class TestMMarco(DatasetIntegrationTest):
             0: TrecQrel('1102432', '2026790', 1, '0'),
             9: TrecQrel('300674', '7067032', 1, '0'),
             59272: TrecQrel('371455', '8009476', 1, '0'),
+        })
+        self._test_qrels('mmarco/pt/dev/small/v1.1', count=7437, items={
+            0: TrecQrel('300674', '7067032', 1, '0'),
+            9: TrecQrel('54544', '7068203', 1, '0'),
+            7436: TrecQrel('195199', '8009377', 1, '0'),
         })
         self._test_qrels('mmarco/de/train', count=532761, items={
             0: TrecQrel('1185869', '0', 1, '0'),
@@ -227,6 +307,11 @@ class TestMMarco(DatasetIntegrationTest):
             9: TrecQrel('300674', '7067032', 1, '0'),
             59272: TrecQrel('371455', '8009476', 1, '0'),
         })
+        self._test_qrels('mmarco/de/dev/small', count=7437, items={
+            0: TrecQrel('300674', '7067032', 1, '0'),
+            9: TrecQrel('54544', '7068203', 1, '0'),
+            7436: TrecQrel('195199', '8009377', 1, '0'),
+        })
         self._test_qrels('mmarco/ru/train', count=532761, items={
             0: TrecQrel('1185869', '0', 1, '0'),
             9: TrecQrel('186154', '1160', 1, '0'),
@@ -236,6 +321,11 @@ class TestMMarco(DatasetIntegrationTest):
             0: TrecQrel('1102432', '2026790', 1, '0'),
             9: TrecQrel('300674', '7067032', 1, '0'),
             59272: TrecQrel('371455', '8009476', 1, '0'),
+        })
+        self._test_qrels('mmarco/ru/dev/small', count=7437, items={
+            0: TrecQrel('300674', '7067032', 1, '0'),
+            9: TrecQrel('54544', '7068203', 1, '0'),
+            7436: TrecQrel('195199', '8009377', 1, '0'),
         })
         self._test_qrels('mmarco/zh/train', count=532761, items={
             0: TrecQrel('1185869', '0', 1, '0'),
@@ -247,11 +337,106 @@ class TestMMarco(DatasetIntegrationTest):
             9: TrecQrel('300674', '7067032', 1, '0'),
             59272: TrecQrel('371455', '8009476', 1, '0'),
         })
+        self._test_qrels('mmarco/zh/dev/small', count=7437, items={
+            0: TrecQrel('300674', '7067032', 1, '0'),
+            9: TrecQrel('54544', '7068203', 1, '0'),
+            7436: TrecQrel('195199', '8009377', 1, '0'),
+        })
         self._test_qrels('mmarco/zh/dev/v1.1', count=59273, items={
             0: TrecQrel('1102432', '2026790', 1, '0'),
             9: TrecQrel('300674', '7067032', 1, '0'),
             59272: TrecQrel('371455', '8009476', 1, '0'),
         })
+        self._test_qrels('mmarco/zh/dev/small/v1.1', count=7437, items={
+            0: TrecQrel('300674', '7067032', 1, '0'),
+            9: TrecQrel('54544', '7068203', 1, '0'),
+            7436: TrecQrel('195199', '8009377', 1, '0'),
+        })
+
+    def test_scoreddocs(self):
+        self._test_scoreddocs('mmarco/es/dev/small', count=6786720, items={
+            0: GenericScoredDoc('2', '1782337', 12.7035),
+            9: GenericScoredDoc('2', '4339073', 11.2667),
+            6786719: GenericScoredDoc('1102400', '533560', 5.598184),
+        })
+        self._test_scoreddocs('mmarco/fr/dev/small', count=6785763, items={
+            0: GenericScoredDoc('2', '1001873', 12.766),
+            9: GenericScoredDoc('2', '6285819', 11.793),
+            6785762: GenericScoredDoc('1102400', '1123059', 5.5288),
+        })
+        self._test_scoreddocs('mmarco/pt/dev/small/v1.1', count=6976324, items={
+            0: GenericScoredDoc('2', '1782337', 13.6133),
+            9: GenericScoredDoc('2', '3996546', 11.7405),
+            6976323: GenericScoredDoc('1102400', '8613556', 6.592),
+        })
+        self._test_scoreddocs('mmarco/it/dev/small', count=6966491, items={
+            0: GenericScoredDoc('2', '1782337', 13.0295),
+            9: GenericScoredDoc('2', '2022784', 11.7611),
+            6966490: GenericScoredDoc('1102400', '4411180', 5.498484),
+        })
+        self._test_scoreddocs('mmarco/id/dev/small', count=6841990, items={
+            0: GenericScoredDoc('2', '1782337', 13.2769),
+            9: GenericScoredDoc('2', '7496504', 11.857),
+            6841989: GenericScoredDoc('1102400', '8018446', 4.710481),
+        })
+        self._test_scoreddocs('mmarco/de/dev/small', count=6594126, items={
+            0: GenericScoredDoc('2', '2022779', 9.7118),
+            9: GenericScoredDoc('2', '2022785', 7.9781),
+            6594125: GenericScoredDoc('1102400', '1954600', 5.524396),
+        })
+        self._test_scoreddocs('mmarco/ru/dev/small', count=6958739, items={
+            0: GenericScoredDoc('2', '6285817', 12.2203),
+            9: GenericScoredDoc('2', '513654', 10.9697),
+            6958738: GenericScoredDoc('1102400', '3863095', 5.499489),
+        })
+        self._test_scoreddocs('mmarco/zh/dev/small/v1.1', count=1034597, items={
+            0: GenericScoredDoc('1215', '6593209', 2.2469),
+            9: GenericScoredDoc('1215', '2815463', 2.237397),
+            1034596: GenericScoredDoc('1102393', '3789102', 5.4537),
+        })
+
+    def test_docpairs(self):
+        self._test_docpairs('mmarco/es/train', count=39780811, items={
+            0: GenericDocPair('400296', '1540783', '3518497'),
+            9: GenericDocPair('189845', '1051356', '4238671'),
+            39780810: GenericDocPair('749547', '394235', '7655192'),
+        })
+        self._test_docpairs('mmarco/fr/train', count=39780811, items={
+            0: GenericDocPair('400296', '1540783', '3518497'),
+            9: GenericDocPair('189845', '1051356', '4238671'),
+            39780810: GenericDocPair('749547', '394235', '7655192'),
+        })
+        self._test_docpairs('mmarco/pt/train', count=39780811, items={
+            0: GenericDocPair('400296', '1540783', '3518497'),
+            9: GenericDocPair('189845', '1051356', '4238671'),
+            39780810: GenericDocPair('749547', '394235', '7655192'),
+        })
+        self._test_docpairs('mmarco/it/train', count=39780811, items={
+            0: GenericDocPair('400296', '1540783', '3518497'),
+            9: GenericDocPair('189845', '1051356', '4238671'),
+            39780810: GenericDocPair('749547', '394235', '7655192'),
+        })
+        self._test_docpairs('mmarco/id/train', count=39780811, items={
+            0: GenericDocPair('400296', '1540783', '3518497'),
+            9: GenericDocPair('189845', '1051356', '4238671'),
+            39780810: GenericDocPair('749547', '394235', '7655192'),
+        })
+        self._test_docpairs('mmarco/de/train', count=39780811, items={
+            0: GenericDocPair('400296', '1540783', '3518497'),
+            9: GenericDocPair('189845', '1051356', '4238671'),
+            39780810: GenericDocPair('749547', '394235', '7655192'),
+        })
+        self._test_docpairs('mmarco/ru/train', count=39780811, items={
+            0: GenericDocPair('400296', '1540783', '3518497'),
+            9: GenericDocPair('189845', '1051356', '4238671'),
+            39780810: GenericDocPair('749547', '394235', '7655192'),
+        })
+        self._test_docpairs('mmarco/zh/train', count=39780811, items={
+            0: GenericDocPair('400296', '1540783', '3518497'),
+            9: GenericDocPair('189845', '1051356', '4238671'),
+            39780810: GenericDocPair('749547', '394235', '7655192'),
+        })
+
 
 if __name__ == '__main__':
     unittest.main()
