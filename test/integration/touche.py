b98a54dbc76dc04d83016b838221044b7fcbd9ae
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-10-05 20:05:53 +0200

    Add Touché tests

--- test/integration/touche.py
+++ test/integration/touche.py
@@ -1,8 +1,10 @@
 from unittest import main
 from re import compile
 
-from ir_datasets.formats import ToucheQuery, TrecQrel, ToucheTitleQuery
-from ir_datasets.formats.touche import ToucheQualityQrel
+from ir_datasets.formats import ToucheQuery, TrecQrel, ToucheTitleQuery, \
+    ToucheComparativeQuery, ToucheQualityQrel, ToucheQualityCoherenceQrel, \
+    ToucheImageDoc, \
+    ToucheQualityComparativeStanceQrel, ToucheComparativeStance, ToucheControversialStanceQrel, ToucheControversialStance
 from test.integration.base import DatasetIntegrationTest
 
 
@@ -114,7 +116,64 @@ class TestTouche(DatasetIntegrationTest):
                 ),
             }
         )
+        self._test_queries(
+            "argsme/2020-04-01-processed/touche-2022-task-1",
+            count=50,
+            items={
+                0: ToucheQuery(
+                    query_id="1",
+                    title="Should teachers get tenure?",
+                    description=compile("A user has heard .{215} professors is of interest."),
+                    narrative=compile("Highly relevant arguments make a clear statement about tenure for teachers in schools or universities. Relevant arguments consider tenure more generally, not specifically for teachers, or, instead of talking about tenure, consider the situation of teachers' financial independence."),
+                ),
+                49: ToucheQuery(
+                    query_id="50",
+                    title="Should everyone get a universal basic income?",
+                    description=compile("Redistribution of wealth is a fundamental concept of many economies and social systems. A key component might be a universal basic income, however, a user wonders whether this truly would help."),
+                    narrative=compile("Highly relevant arguments take a clear stance toward the universal basic income, giving clear premises. Relevant arguments offer only emotional arguments, or talk about minimum wages, mentioning universal basic income only in passing."),
+                ),
+            }
+        )
+        self._test_queries(
+            "clueweb12/touche-2022-task-2",
+            count=50,
+            items={
+                0: ToucheComparativeQuery(
+                    query_id="2",
+                    title="Which is better, a laptop or a desktop?",
+                    objects=("laptop", "desktop"),
+                    description=compile("A user wants to buy .{272} gaming desktop PC."),
+                    narrative=compile("Highly relevant documents will describe the major similarities and dissimilarities of laptops and desktops along with the respective advantages and disadvantages of specific usage scenarios. A comparison of the technical and architectural characteristics without personal opinion, recommendation, or pros/cons is not relevant."),
+                ),
+                49: ToucheComparativeQuery(
+                    query_id="100",
+                    title="Should I learn Python or R for data analysis?",
+                    objects=("Python", "R"),
+                    description=compile("Wondering whether you should use Python or R for data analysis\? It's hard to know whether to use Python or R for data analysis. And that's especially true if you're a newbie data analyst looking for the right language to start with. Users are looking for documents that help them decide which programming language, Python or R, is better suited for data analysis, is more useful, flexible, easy to learn and efficient."),
+                    narrative=compile("Relevant documents should compare two programming languages for data analysis: Python and R. Highly relevant documents should compare the two objects in terms of efficiency, usefulness, easiness to learn and so on. Highly relevant documents would ideally compare most of the features. The documents that only describe one language, but provide insights on how it is good for data analysis are relevant. Arguments and opinions of using one of the two or both for other tasks that are not related to data analysis, are not relevant."),
+                ),
+            }
+        )
+        self._test_queries(
+            "touche/touche-2022-task-3",
+            count=50,
+            items={
+                0: ToucheQuery(
+                    query_id="1",
+                    title="Should teachers get tenure?",
+                    description=compile("A user has heard .{215} professors is of interest."),
+                    narrative=compile("Highly relevant arguments make a clear statement about tenure for teachers in schools or universities. Relevant arguments consider tenure more generally, not specifically for teachers, or, instead of talking about tenure, consider the situation of teachers' financial independence."),
+                ),
+                49: ToucheQuery(
+                    query_id="50",
+                    title="Should everyone get a universal basic income?",
+                    description=compile("Redistribution of wealth is a fundamental concept of many economies and social systems. A key component might be a universal basic income, however, a user wonders whether this truly would help."),
+                    narrative=compile("Highly relevant arguments take a clear stance toward the universal basic income, giving clear premises. Relevant arguments offer only emotional arguments, or talk about minimum wages, mentioning universal basic income only in passing."),
+                ),
+            }
+        )
 
+    # noinspection PyTypeChecker
     def test_qrels(self):
         self._test_qrels(
             "argsme/2020-04-01/touche-2020-task-1",
@@ -228,6 +287,89 @@ class TestTouche(DatasetIntegrationTest):
                 ),
             }
         )
+        self._test_qrels(
+            "argsme/2020-04-01-processed/touche-2022-task-1",
+            count=6841,
+            items={
+                0: ToucheQualityCoherenceQrel(
+                    query_id="1",
+                    doc_id="Sc065954f-Ae72bc9c6__PREMISE__41,Sc065954f-Ae72bc9c6__CONC__1",
+                    relevance=2,
+                    quality=2,
+                    coherence=2,
+                    iteration="0"
+                ),
+                6840: ToucheQualityCoherenceQrel(
+                    query_id="50",
+                    doc_id="Sffdf2e2e-A20e9dd06__PREMISE__4,Sffdf2e2e-A20e9dd06__PREMISE__5",
+                    relevance=1,
+                    quality=1,
+                    coherence=1,
+                    iteration="0"
+                ),
+            }
+        )
+        self._test_qrels(
+            "clueweb12/touche-2022-task-2",
+            count=2107,
+            items={
+                0: ToucheQualityComparativeStanceQrel(
+                    query_id="12",
+                    doc_id="clueweb12-0002wb-18-34442___2",
+                    relevance=0,
+                    quality=2,
+                    stance=ToucheComparativeStance.NO,
+                    iteration="0"
+                ),
+                2106: ToucheQualityComparativeStanceQrel(
+                    query_id="70",
+                    doc_id="clueweb12-1900tw-42-07368___7",
+                    relevance=1,
+                    quality=1,
+                    stance=ToucheComparativeStance.NO,
+                    iteration="0"
+                ),
+            }
+        )
+        self._test_qrels(
+            "touche/touche-2022-task-3",
+            count=19821,
+            items={
+                0: ToucheControversialStanceQrel(
+                    query_id="1",
+                    doc_id="Ib7fc7d5f8ee59d62",
+                    relevance=1,
+                    stance=ToucheControversialStance.ONTOPIC,
+                ),
+                19820: ToucheControversialStanceQrel(
+                    query_id="50",
+                    doc_id="I490ab3908d308757",
+                    relevance=1,
+                    stance=ToucheControversialStance.CON,
+                ),
+            }
+        )
+
+    # noinspection PyTypeChecker
+    def test_docs(self):
+        self._test_docs("touche/touche-2022-task-3", count=23841, items={
+            0: ToucheImageDoc(
+                doc_id="I000330ba4ea0ad13",
+                png=compile(b"\x89PNG.*"),
+                webp=compile(b"RIFF\xd0\xf3\x05\x00WEBPVP8.*"),
+                url="https://www.e-dmj.org/upload//thumbnails/dmj-2020-0258f3.jpg",
+                phash="1000000011001011011101010011101010010111011010101000011101101100",
+                pages=[]
+            ),
+            23840: ToucheImageDoc(
+                doc_id="Iffff8be6926a808e",
+                png=compile(b"\x89PNG.*"),
+                webp=compile(b"RIFF\x0e\\+\x00\x00WEBPVP8.*"),
+                url="https://assets.pewresearch.org/wp-content/uploads/sites/11/2012/07/death-penalty-2011-1.png",
+                phash="0001011011111110101001010100010110101011101000101111000001110000",
+                pages=[]
+            ),
+        })
 
 
 if __name__ == "__main__":
