05d710d8e7da0d200c3fe00ff9f2133d02503400
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-10-19 23:18:16 +0200

    Add CW22 base records

---
+++ ir_datasets/formats/clueweb22.py
@@ -0,0 +1,72 @@
+from datetime import datetime
+from typing import NamedTuple, Sequence
+
+
+class _Txt(NamedTuple):
+    """
+    Record from the ``txt`` subdir.
+    """
+    doc_id: str
+    url: str
+    url_hash: str
+    language: str
+    text: str
+
+
+class _Html(NamedTuple):
+    """
+    Record from the ``html`` subdir.
+    """
+    doc_id: str
+    url: str
+    url_hash: str
+    language: str
+    date: datetime
+    html: bytes
+    vdom_heading: Sequence[int]
+    vdom_list: Sequence[int]
+    vdom_passage: Sequence[int]
+    vdom_primary: Sequence[int]
+    vdom_table: Sequence[int]
+    vdom_title: Sequence[int]
+    vdom_paragraph: Sequence[int]
+
+
+class Anchor(NamedTuple):
+    """
+    Anchor sub-record from the ``inlink`` and ``outlink`` subdirs.
+    """
+    url: str
+    url_hash: str
+    text: str
+    language: str
+
+
+class _Link(NamedTuple):
+    """
+    Record from the ``inlink`` and ``outlink`` subdirs.
+    """
+    doc_id: str
+    url: str
+    url_hash: str
+    anchors: Sequence[Anchor]
+
+
+class _Vdom(NamedTuple):
+    """
+    Record from the ``vdom`` subdirs.
+    """
+    doc_id: str
+    url: str
+    url_hash: str
+    # TODO how to parse?
+
+
+class _Jpg(NamedTuple):
+    """
+    Record from the ``jpg`` subdirs.
+    """
+    doc_id: str
+    url: str
+    url_hash: str
+    # TODO how to parse?
