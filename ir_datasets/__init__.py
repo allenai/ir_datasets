4e4669361530bab035bde4e43ca00861727776ed
Sean MacAvaney <sean.macavaney@gmail.com>
2024-02-04 15:59:27 +0000

    fix location of msmarco source files and bump version (#257)

--- ir_datasets/__init__.py
+++ ir_datasets/__init__.py
@@ -101,4 +101,4 @@ def main_cli():
     import sys
     main(sys.argv[1:])
 
-__version__ = "0.5.5" # NOTE: keep this in sync with setup.py
+__version__ = "0.5.6" # NOTE: keep this in sync with setup.py
