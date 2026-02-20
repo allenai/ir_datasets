02a8f21c0f51d330124c30f1257d3fcb5866f3eb
Jack McKechnie <46594345+JackMcKechnie@users.noreply.github.com>
2026-02-20 16:52:06 +0000

    feat: update initial sara dataset topics, docs and qrels

--- ir_datasets/datasets/sara.py
+++ ir_datasets/datasets/sara.py
@@ -1,20 +1,11 @@
 import ir_datasets
-from ir_datasets.formats import TsvDocs, TsvQueries, TrecQrels
-from ir_datasets.util import DownloadConfig, TarExtract, Cache
 from ir_datasets.datasets.base import Dataset, YamlDocumentation
-from ir_datasets.datasets.base import Dataset, FilteredQueries, FilteredQrels, YamlDocumentation
-from ir_datasets.formats import BaseDocs, TrecXmlQueries, TrecQrels, GenericQuery, GenericQrel
+from ir_datasets.formats import BaseDocs, TsvQueries, TrecQrels
 from ir_datasets.indices import PickleLz4FullStore
-from typing import NamedTuple, Tuple
-import itertools
+from ir_datasets.util import DownloadConfig
+from typing import NamedTuple
+import csv
 import io
-from ir_datasets.util import GzipExtract, Cache, Lazy, TarExtractAll, RelativePath
-import os
-import re
-import logging
-import glob
-import tarfile
-
 
 # A unique identifier for this dataset. This should match the file name (with "-" instead of "_")
 NAME = "sara"
@@ -42,26 +33,16 @@ class SaraDocs(BaseDocs):
         return iter(self.docs_store())
 
     def _docs_iter(self):
-        with self._dlc.stream() as stream, \
-             tarfile.open(fileobj=stream, mode='r|gz') as tarf:
-            it = iter(tarf)
-            for record in it:
-                if record.name.endswith('.txt') and not record.name.endswith('categories.txt'):
-                    dir = os.path.basename(record.name)
-                    doc_id = os.path.splitext(dir)[0]
-                    with tarf.extractfile(record) as inp:
-                         contents = inp.read().decode()
-                    record = next(it)
-                    assert record.name.endswith(f'{doc_id}.cats')
-                    sensitive = 0
-                    with tarf.extractfile(record) as file:
-                        for line in file:
-                            line = line.split()[0]
-                            line = line.split(b",")
-                            if int(line[0]) == 1 and int(line[1]) in (2, 3):
-                                sensitive = 1
-                    yield SaraDoc(doc_id, contents, sensitive)
-
+            # Stream the CSV directly from DLC
+            with self._dlc.stream() as stream:
+                text_stream = io.TextIOWrapper(stream, encoding="utf-8")
+                reader = csv.DictReader(text_stream)
+                for row in reader:
+                    yield SaraDoc(
+                        doc_id=row["docno"],
+                        text=row["text"],
+                        sensitivity=int(row["sensitivity"])
+                    )
     def docs_store(self, field='doc_id'):
         return PickleLz4FullStore(
             path=f'{ir_datasets.util.home_path()/NAME}/docs.pklz4',
